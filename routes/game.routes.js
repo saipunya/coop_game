edf4eb90e62ac7ff42b50346da22b455bc0a40b6
saipunya <116610845+saipunya@users.noreply.github.com>
2026-04-03 23:44:36 +0700

    Update game.routes.js

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const gameController = require('../controllers/game.controller');
 
 router.get('/', gameController.showStartPage);
+router.get('/play', gameController.showPlayPage);
+router.post('/api/start', gameController.startGame);
 
 module.exports = router;
