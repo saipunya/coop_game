83b0b4355666b3376189e6bf8a909184db8740e3
saipunya <116610845+saipunya@users.noreply.github.com>
2026-04-03 23:43:33 +0700

    Update game.controller.js

--- controllers/game.controller.js
+++ controllers/game.controller.js
@@ -1,3 +1,15 @@
 exports.showStartPage = (req, res) => {
   res.render('game/start');
 };
+
+exports.showPlayPage = (req, res) => {
+  res.render('game/play');
+};
+
+exports.startGame = (req, res) => {
+  return res.json({
+    success: true,
+    attemptId: Date.now(),
+    message: 'เริ่มเกมสำเร็จ'
+  });
+};
